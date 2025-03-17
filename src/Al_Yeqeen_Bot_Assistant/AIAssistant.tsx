23d1d73ad739c3485ce4a6a4e1e52601c48f8658
SuhudAlyeqeenDotGithub <alyekeenlearningzone@outlook.com>
2025-03-17 18:14:17 +0000

    Took out api log

--- src/Al_Yeqeen_Bot_Assistant/AIAssistant.tsx
+++ src/Al_Yeqeen_Bot_Assistant/AIAssistant.tsx
@@ -34,8 +34,6 @@ function AIAssistant() {
   };
 
   const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-  console.log("api key1", process.env.NEXT_PUBLIC_OPENAI_API_KEY);
-  console.log("api key2", apiKey);
 
   const fetchData = async () => {
     setFetchingResponse(true);
